b8057245a44410883f25e0705d4fc9e7067d92f2
riyadhctg <3258642+riyadhctg@users.noreply.github.com>
2022-08-02 17:31:40 +0800

    Update data.js

--- data.js
+++ data.js
@@ -18,5 +18,10 @@ const data = [
       "title": "Golang",
       "subtitle": "Can we extend Golang struct?",
       "description": `There is no extension in the "classical" OOP sense, embedding a type in a struct will not add fields of the embedded struct but add a single field with a type being the embedded type`
+  },
+  {
+      "title": "Linux / Scripts",
+      "subtitle": "shebang",
+      "description": `You may have seen this type of heading in scripts --> #!/bin/bash. It's called shebang. A shebang is only relevant when a script has the execute permission (e.g. chmod u+x script.sh). Since it's commented out, it does not typically interfere with the script. When a shell executes the script it will use the specified interpreter. It's typically in this format: #!interpreter [optional-arg]`
   }
 ]
