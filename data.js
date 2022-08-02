920bc5731134a14cc778d3e9fc74bbf44df78ade
riyadhctg <3258642+riyadhctg@users.noreply.github.com>
2022-08-02 15:24:38 +0800

    test

--- data.js
+++ data.js
@@ -13,5 +13,10 @@ const data = [
       "title": "Git Command",
       "subtitle": "git checkout -b new_branch_name",
       "description": "Create a new git branch in local named new_branch_name"
+  },
+  {
+      "title": "Golang",
+      "subtitle": "Can we extend Golang struct?",
+      "description": `There is no extension in the "classical" OOP sense, embedding a type in a struct will not add fields of the embedded struct but add a single field with a type being the embedded type`
   }
 ]
