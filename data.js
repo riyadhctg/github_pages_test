339dbfcd6d9c72933cdc1e03b3966f861aedb10b
riyadhctg <3258642+riyadhctg@users.noreply.github.com>
2022-08-06 09:44:35 +0800

    Update data.js

--- data.js
+++ data.js
@@ -31,7 +31,7 @@ const data = [
   },
   {
       "title": "Golang",
-      "subtitle": "go mod init <module path>",
+      "subtitle": `go mod init <module path>`,
       "description": ` When your code imports packages contained in other modules, you manage those dependencies through your code's own module. That module is defined by a go.mod file that tracks the modules that provide those packages. That go.mod file stays with your code, including in your source code repository.
       
       To enable dependency tracking for your code by creating a go.mod file, run the go mod init command, giving it the name of the module your code will be in. The name is the module's module path.`
