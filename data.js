d07e1b23318c76f1ee08ba729cb6cc40efb3e97a
riyadhctg <3258642+riyadhctg@users.noreply.github.com>
2022-09-02 11:31:23 +0800

    Update data.js

--- data.js
+++ data.js
@@ -42,6 +42,101 @@ const data = [
       "description": `This will revert everything from the HEAD back to the commit hash, meaning it will recreate that commit state in the working tree as if every commit after 0766c053 had been walked back. You can then commit the current tree, and it will create a brand new commit essentially equivalent to the commit you "reverted" to.
 	    The --no-commit flag lets git revert all the commits at once- otherwise you'll be prompted for a message for each commit in the range, littering your history with unnecessary new commits.
 	    This is a safe and easy way to rollback to a previous state. No history is destroyed, so it can be used for commits that have already been made public. Source: https://stackoverflow.com/a/21718540`
+  },
+	  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Be humble",
+      "description": `No engineer in the world knows everything, the same applies to you. Source: https://ondrejsevcik.com/blog/20-rules-in-10-year. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Make it work, make it right.",
+      "description": `(and sometimes make it fast). Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Optimize for change",
+      "description": `Prefer duplication over the wrong abstraction. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Always write tests",
+      "description": `If you're not writing tests, you're testing manually. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },  
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Prefer functional programming",
+      "description": `It's easier to understand. If your code requires a Ph.D. to understand, you’re most likely doing it wrong. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Delete as much code as you can",
+      "description": `That's it. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Good Enough is Better than Perfect -",
+      "description": ` Don't discard meaningful improvement just because it's not perfect. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Criticize in private, praise in public",
+      "description": `That's it. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Make notes",
+      "description": `If you think you will remember it, you're lying to yourself. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Talk to your users",
+      "description": `The best software is built by engineers who have empathy for their users. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Learn with intention",
+      "description": `Practice with a clear and specific goal in mindwhat you want to improve and exactly how (deliberate practice). Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Do not generalize early",
+      "description": `Wait until you have at least 3 duplicates of code before you make an abstraction (aka. Rule Of Three). Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Fix broken windows",
+      "description": `One hack in code leads to another hack. Soon your code will become unmanageable. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Fix the problem",
+      "description": `doesn't matter whose fault it is, it is your problem. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Do what works, not what's fashionable",
+      "description": `Try it first with a small team. If it works, expand. If not, abort. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "The best work requires a good rest",
+      "description": `Regular time off is essential for top performance. You also don’t expect professional sprinters to sprint all the time. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Take small steps",
+      "description": `Big rewrites don't work. You will lose motivation and focus along the way. Aim to release daily. It gives you the freedom to change focus when necessary. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Praise for good work",
+      "description": `Something we've observed in animals, but works for people too. You will get better results when you praise people for good work rather than punishing them for a bad one. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
+  },
+  {
+      "title": "Principles / Tips for Software Development",
+      "subtitle": "Perfect code doesn't exist",
+      "description": `It's better to accept this as a fact rather than wasting time and chasing the impossible. Source: https://ondrejsevcik.com/blog/20-rules-in-10-years`
   }
   
 ]
